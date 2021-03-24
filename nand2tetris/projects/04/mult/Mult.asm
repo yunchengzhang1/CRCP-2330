0f0f84895ba7b7d5bd3633d4855b2d16fdf0f9ab
yunchengzhang1 <yunchengz@smu.edu>
2021-03-23 20:10:43 -0500

    error: line 14 expression expected

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,8 +10,35 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+	//set up r0 and r1
+	@R0
+	M=2		//R0 as the counter
+	@R1
+	M=5	
+	
+(LOOP)
+	//if R0 the counter is 0, skip line 37 and go to END 
+	@R0
+	D=M     //D=R0
+	@END
+	D;JEQ
 
-@2
-D=A
+	//increment R2 the result address with R1
+	@R1
+	D=M 	//D=R1
+	@R2
+	M=M+D 	//R2=R2+R1
+
+	//decrement R0 as the counter
+	@R0
+	M=M-1   //R0=R0-1
+
+	//back to loop in any condition
+	0;JMP
+
+(END)
+	@END
+	0;JMP	//infinite loop 
+
 
 

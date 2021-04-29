605033114b41ce988be8bc15bc5eba3c14b1b7ab
yunchengzhang1 <yunchengz@smu.edu>
2021-04-28 19:48:56 -0500

    code outline

--- nand2tetris/projects/06/assembler/code.h
+++ nand2tetris/projects/06/assembler/code.h
@@ -7,5 +7,27 @@
 #include <iostream>
 #ifndef INC_06_CODE_H
 #define INC_06_CODE_H
+using namespace std;
+class Code {
 
+public:
+//    front
+    Code();
+
+    uint16_t 	comp(const std::string&) const;
+
+    uint8_t 	dest(const std::string&) const;
+
+    uint8_t 	jump(const std::string&) const;
+
+    uint16_t 	immediate(const std::string&) const;
+
+private:
+//    back
+    std::unordered_map<std::string, uint16_t> opcodes_;
+
+    std::unordered_map<std::string, uint8_t> jumps_;
+
+    std::unordered_map<std::string, uint8_t> dests_;
+};
 #endif //INC_06_CODE_H
